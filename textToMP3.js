14984f9266dba8da45e009f013551df0a026940e
MinjeongKim03 <94886176+MinjeongKim03@users.noreply.github.com>
2021-11-30 14:28:03 +0900

    Update textToMP3.js

--- textToMP3.js
+++ textToMP3.js
@@ -1,5 +1,5 @@
-AWS.config.region = 'us-east-1';
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'us-east-1:d7570e49-67ce-4e1b-850e-e65e4abac9f0'});
+AWS.config.region = '';
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: ''});
 
 function speakText(detectedTXT, code) {
     const voices = {
